9f81106954ebdb8922243670fd2ca8b2aad54f23
rocky0shao <rockyshao22@gmail.com>
2025-12-28 17:25:18 -0500

    Fix Task Title/Description order error

--- app/actions.ts
+++ app/actions.ts
@@ -2,7 +2,6 @@
 import { getTasksFromDB, addTaskToDB, toggleTaskInDB } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 export async function handleAddTask(userId: string, taskTitle: string, taskDescription: string =''){
-    userId = "b9c4569d-bd49-43e0-91ba-aa119a6d14fc"
     await addTaskToDB(userId, taskTitle, taskDescription)
     revalidatePath('/')
 }
