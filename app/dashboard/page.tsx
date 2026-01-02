81d1e6f694bf94556ca65c861dbe7befeb8e7dd2
rocky0shao <rockyshao22@gmail.com>
2026-01-02 14:00:21 -0500

    Fix git issue. App working. Readme finished

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,26 +2,15 @@
 import { TaskItem } from '@/components/TaskItem'
 import { Title } from '@/components/Title'
 import { NewTaskForm } from '@/components/NewTaskForm'
-import { getTasksFromDB, createClient } from '@/utils/supabase/server' // Import DB fetcher directly
+import { getTasksFromDB } from '@/utils/supabase/server' // Import DB fetcher directly
 
 export default async function Home() {
-  const supabase = createClient()
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
   const tasks = await getTasksFromDB()
-  const completedTasks = tasks.filter(task => task.is_completed).length
-  const remainingTasks = tasks.length - completedTasks
 
   return (
     <main>
       <Title />
-      {user && (
-        <div className="text-center mb-4">
-          <p className="text-lg">Welcome, {user.email}</p>
-          <p>You have {remainingTasks} tasks remaining and {completedTasks} tasks completed.</p>
-        </div>
-      )}
+
       <NewTaskForm />
       <ul>
         {tasks.map(task => (
